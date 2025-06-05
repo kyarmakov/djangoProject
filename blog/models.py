976a8bff2389e5255037286e2ec251c99492adc3
kr.yarmakov <kr.yarmakov@tensor.ru>
2025-06-05 18:05:23 +0300

    add models for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    datetime = models.DateTimeField(u'Publishing Date', default=timezone.now)
+    content = models.TextField(max_length=10000)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author_name} on {self.post}'
